48372ecca583b3817c03fea2b022a3dd2fe3b05a
pramesywara_j <pramesywara.jembar@efishery.com>
2025-01-29 11:49:35 +0700

    chore: change entities path

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { config } from 'dotenv';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { join } from 'path';
 
 config();
 
@@ -19,6 +20,7 @@ const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
       username: DB_USERNAME,
       password: DB_PASSWORD,
       database: DB_NAME,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
     }),
     AuthModule,
     UserModule,
